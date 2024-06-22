eee8dbb761821e0337ea02325007de4b89b9e5f4
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2024-06-22 18:00:26 +1200

    Started on map code

--- src/pages/FindStation/FindStation.jsx
+++ src/pages/FindStation/FindStation.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/Footer/Footer";
+import StationMap from "../../components/StationMap/StationMap";
 
 export default function FindStation() {
   return (
     <div>
       <Header />
+      <StationMap />
       <Footer />
     </div>
   );
