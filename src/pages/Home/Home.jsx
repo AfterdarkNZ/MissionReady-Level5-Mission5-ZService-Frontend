1455c6f32becd55770f0101e83e775e51900dc35
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-06-24 10:22:18 +1200

    Styled first info section and made infosection component.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,32 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/Footer/Footer";
 import TitleSection from "./components/TitleSection";
+import InfoSections from "./components/InfoSections";
+
+// import styling
+import styles from "./Home.module.css";
+
+// image imports
+import evAtHome from "../../assets/images/ev-at-home.png";
+import fuelBackPlan from "../../assets/images/fuel-back-plan.png";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <TitleSection />
+      <InfoSections
+        title="Power your home"
+        paragraph="Specialised home power plans to help keep your home and vehicle running, tailored for both EV and non-EV drivers."
+        btnInfo="Find out more"
+      >
+        <img src={evAtHome} className={styles.rightImg} alt="Ev at home" />
+        <img
+          src={fuelBackPlan}
+          className={styles.rightImg}
+          alt="Fuel back plan"
+        />
+      </InfoSections>
       <Footer />
     </div>
   );
---
+++ src/pages/Home/components/InfoSections.jsx
@@ -0,0 +1,25 @@
+import styles from "../Home.module.css";
+import { ArrowRight } from "@phosphor-icons/react";
+
+export default function InfoSections(props) {
+  return (
+    <div className={styles.flex}>
+      <div className={styles.left}>
+        <h2 className={styles.sectionTitle}>{props.title}</h2>
+        <p className={styles.sectionParagraph}>{props.paragraph}</p>
+        <button className={styles.sectionBtn}>
+          <p className={styles.btnText}>{props.btnInfo}</p>
+          <div className={styles.arrowDiv}>
+            <ArrowRight
+              size={24}
+              color="#ed560e"
+              weight="bold"
+              className={styles.arrowIcon}
+            />
+          </div>
+        </button>
+      </div>
+      <div className={styles.right}>{props.children}</div>
+    </div>
+  );
+}
